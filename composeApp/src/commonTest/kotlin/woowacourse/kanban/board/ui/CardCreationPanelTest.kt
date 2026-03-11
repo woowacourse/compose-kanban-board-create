8f6a3040b013ff9f6cdfe49759d83fdbe7bdf224
todays_sun_day <178suna@gmail.com>
2026-03-11 18:23:43 +0900

    feat: CardCreationPanel UI 테스트 로직 작성

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/CardCreationPanelTest.kt
@@ -0,0 +1,64 @@
+package woowacourse.kanban.board.ui
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.runComposeUiTest
+import org.junit.Test
+import woowacourse.kanban.board.domain.CardData
+
+@OptIn(ExperimentalTestApi::class)
+class CardCreationPanelTest {
+
+    @Test
+    fun `제목을 입력하지 않으면, 생성 버튼이 비활성화된다`() = runComposeUiTest {
+        // given
+        val blankTitle = ""
+
+        //when
+        setContent {
+            CardCreationPanel(
+                onAddItem = { CardData.create(blankTitle, "", listOf(("")), "구름") },
+                onShowCardCreationPanel = {},
+            )
+        }
+
+        //then
+        onNodeWithText("생성").assertIsNotEnabled()
+    }
+
+    @Test
+    fun `제목을 입력하지 않으면 에러메시지가 노출된다`() = runComposeUiTest {
+        // given
+        val blankTitle = ""
+
+        //when
+        setContent {
+            CardCreationPanel(
+                onAddItem = { CardData.create(blankTitle, "", listOf(("")), "구름") },
+                onShowCardCreationPanel = {},
+            )
+        }
+
+        //then
+        onNodeWithText("제목을 입력해 주세요.").assertExists()
+    }
+
+    @Test
+    fun `태그를 잘못 입력하면, 생성 버튼이 비활성화된다`() = runComposeUiTest {
+        // given
+        val wrongTag = ",태그"
+
+        //when
+        setContent {
+            CardCreationPanel(
+                onAddItem = { CardData.create("제목", "", wrongTag.split(","), "구름") },
+                onShowCardCreationPanel = {},
+            )
+        }
+
+        //then
+        onNodeWithText("생성").assertIsNotEnabled()
+    }
+}

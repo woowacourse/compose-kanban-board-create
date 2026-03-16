7ab562960618278c2b47e89de079d179f110f079
yws <dbdntjs620@gmail.com>
2026-03-16 13:21:12 +0900

    refactor(taskcarddatainput): 컴포넌트 재정의

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/modal/TaskCardDataInput.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/modal/TaskCardDataInput.kt
@@ -1,14 +1,22 @@
 package woowacourse.kanban.board.component.modal
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
@@ -18,8 +26,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import woowacourse.kanban.board.Blue50
 import woowacourse.kanban.board.ComponentText
 import woowacourse.kanban.board.ComponentText.DESCRIPTION_LABEL
 import woowacourse.kanban.board.ComponentText.DESCRIPTION_PLACEHOLDER
@@ -37,10 +48,11 @@ import woowacourse.kanban.board.ComponentText.TAG_PLACEHOLDER
 import woowacourse.kanban.board.ComponentText.TAG_SUPPORTING
 import woowacourse.kanban.board.ComponentText.TITLE_ERROR
 import woowacourse.kanban.board.ComponentText.TITLE_PLACEHOLDER
+import woowacourse.kanban.board.Gray20
 import woowacourse.kanban.board.Validator
 
 @Composable
-fun Modal() {
+fun TaskCardDataInput() {
     var title by remember { mutableStateOf("") }
     var description by remember { mutableStateOf("") }
     var tags by remember { mutableStateOf("") }
@@ -83,7 +95,26 @@ fun Modal() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(24.dp),
         ) {
-            Header()
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .padding(horizontal = 24.dp)
+                    .fillMaxWidth(),
+            ) {
+                Text(
+                    text = ComponentText.HEADER_LABEL,
+                    fontSize = 20.sp,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                )
+                Icon(
+                    modifier = Modifier
+                        .clickable {},
+                    imageVector = Icons.Default.Close,
+                    contentDescription = "닫기",
+                )
+            }
 
             HorizontalDivider()
 
@@ -155,17 +186,41 @@ fun Modal() {
                 }
             }
 
-            Footer(
-                isButtonEnabled = Validator.checkButtonEnable(isTitleEmpty, isNotValidTag),
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp),
+            ) {
+                HorizontalDivider()
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
+                ) {
+                    FooterButton(
+                        containerColor = Color.Transparent,
+                        contentColor = Gray20,
+                        text = ComponentText.CANCEL_BUTTON,
+                    )
+
+                    Spacer(modifier = Modifier.width(12.dp))
+
+                    FooterButton(
+                        enabled = !isTitleEmpty && !isNotValidTag,
+                        containerColor = Blue50,
+                        text = ComponentText.CREATE_BUTTON,
+                    )
+                }
+            }
         }
     }
 }
 
 @Preview(showBackground = true, widthDp = 800, heightDp = 1000)
 @Composable
-private fun ModalPreview() {
-    Modal()
+private fun TaskCardDataInputPreview() {
+    TaskCardDataInput()
 }
 
 //@Preview(showBackground = true)

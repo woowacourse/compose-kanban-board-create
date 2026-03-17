1c17e594a28d2b9678ce0d53e7b18d15107ef263
joon0447 <joon0447@naver.com>
2026-03-17 15:22:21 +0900

    refactor(appcolors): internal 삭제

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/AppColors.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/AppColors.kt
@@ -2,12 +2,12 @@ package woowacourse.kanban.board
 
 import androidx.compose.ui.graphics.Color
 
-internal val Gray10 = Color(0xFF101828)
-internal val Gray20 = Color(0xff364153)
-internal val Gray30 = Color(0xFF4A5565)
-internal val Gray40 = Color(0xFFAAAAAA)
-internal val Gray70 = Color(0xFFE5E7EB)
-internal val Gray80 = Color(0xFFF3F4F6)
-internal val Blue50 = Color(0xFF1447E6)
-internal val Blue80 = Color(0xFFEEF2FF)
-internal val Red50 = Color(0xFFB3261E)
+val Gray10 = Color(0xFF101828)
+val Gray20 = Color(0xff364153)
+val Gray30 = Color(0xFF4A5565)
+val Gray40 = Color(0xFFAAAAAA)
+val Gray70 = Color(0xFFE5E7EB)
+val Gray80 = Color(0xFFF3F4F6)
+val Blue50 = Color(0xFF1447E6)
+val Blue80 = Color(0xFFEEF2FF)
+val Red50 = Color(0xFFB3261E)

ce99456c6ec83914880ba4e613c51ebce9ec0c65
yws <dbdntjs620@gmail.com>
2026-03-17 17:52:11 +0900

    feat(testtext): 테스트 상수 추가

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/testconstan/TestText.kt
@@ -0,0 +1,16 @@
+package woowacourse.kanban.board.testconstan
+
+object TestText {
+
+    const val EXAMPLE_TEXT = "example"
+
+    const val BLANK_TEXT = ""
+
+    const val TEST_TITLE = "test title"
+
+    const val TEST_TAGS = "tag1, tag2, tag3, tag4, tag5"
+
+    const val TOO_MANY_TAGS = "tag1, tag2, tag3, tag4, tag5, tag6,"
+
+    const val TOO_LONG_TAG = "tag1, tag2, tag3, tooLongTag"
+}

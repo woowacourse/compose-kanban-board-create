1181a421122b9aef3649533b1feb9658f7c50f69
yws <dbdntjs620@gmail.com>
2026-03-17 15:59:24 +0900

    feat(testtags): testtags 상수 추가

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/constant/TestTags.kt
@@ -0,0 +1,13 @@
+package woowacourse.kanban.board.constant
+
+object TestTags {
+    const val MODAL_BODY = "modalBody"
+    const val HEADER = "header"
+    const val TITLE_INPUT = "titleInput"
+    const val DESCRIPTION_INPUT = "descriptionInput"
+    const val TAGS_INPUT = "tagInput"
+    const val STATE_BTN = "stateBtn"
+    const val MANAGER_BTN = "managerBtn"
+    const val BTN = "Btn"
+    const val FOOTER = "footer"
+}

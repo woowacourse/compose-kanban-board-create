94caad607d9170eac7c11a8a86ab439426e645cd
joon0447 <joon0447@naver.com>
2026-03-17 15:59:58 +0900

    refactor(footer): FooterButton를 Footer로 이동

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/modal/Footer.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/modal/Footer.kt
@@ -8,12 +8,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import woowacourse.kanban.board.Blue50
 import woowacourse.kanban.board.ComponentText
 import woowacourse.kanban.board.Gray20
@@ -44,6 +51,33 @@ fun Footer(isButtonEnabled: Boolean, modifier: Modifier = Modifier) {
                 text = ComponentText.CREATE_BUTTON,
             )
         }
+    }
+}
+
+@Composable
+private fun FooterButton(
+    containerColor: Color,
+    text: String,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    onClick: () -> Unit = {},
+    contentColor: Color = Color.Unspecified,
+) {
+    Button(
+        enabled = enabled,
+        onClick = onClick,
+        modifier = modifier
+            .clip(RoundedCornerShape(10.dp)),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = containerColor,
+            contentColor = contentColor,
+        ),
+    ) {
+        Text(
+            text = text,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Normal,
+        )
     }
 }
 

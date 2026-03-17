1226dd7d08552f586e394b1b08983bd892a4ed8a
yws <dbdntjs620@gmail.com>
2026-03-17 17:48:32 +0900

    feat(titletest): Title 단위 테스트 추가

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/unittest/TitleTest.kt
@@ -0,0 +1,32 @@
+package woowacourse.kanban.board.unittest
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import woowacourse.kanban.board.model.Title
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+
+class TitleTest {
+
+    @Test
+    fun `제목에 아무것도 입력되지 않으면 isNotValidTitle이 true를 반환한다`() {
+        val title = Title("")
+
+        assertTrue(title.isNotValidTitle())
+    }
+
+    @Test
+    fun `제목에 공백만 입력되면 isNotValidTitle이 true를 반환한다`() {
+        val title = Title("        ")
+
+        assertTrue(title.isNotValidTitle())
+    }
+
+    @Test
+    fun `제목에 문자가 입력되면 isNotValidTitle이 false를 반환한다`() {
+        val title = Title("title")
+
+        assertFalse(title.isNotValidTitle())
+    }
+}

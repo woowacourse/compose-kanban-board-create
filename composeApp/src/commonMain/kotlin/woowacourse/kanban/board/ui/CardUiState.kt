1cb154031aaa888105f7691d0eec1fe106663ba5
todays_sun_day <178suna@gmail.com>
2026-03-19 17:24:48 +0900

    feat: CardData 의 순수 데이터만 가지는 CardUiState 생성

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/CardUiState.kt
@@ -0,0 +1,22 @@
+package woowacourse.kanban.board.ui
+
+import woowacourse.kanban.board.domain.CardData
+import woowacourse.kanban.board.domain.TaskState
+
+data class CardUiState(
+    val title: String,
+    val description: String,
+    val tags: List<String>,
+    val state: TaskState,
+    val managerName: String
+)
+
+fun CardData.toUiState(): CardUiState {
+    return CardUiState(
+        title = this.title,
+        description = this.description,
+        tags = this.tags,
+        state = this.state,
+        managerName = this.managerName
+    )
+}
